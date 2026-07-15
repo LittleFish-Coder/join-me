6f8ac2a99083aa71eb93b150781c517511411f03
Merliah <merliah.ding@gmail.com>
2026-07-15 13:45:51 +0800

    Initial Commit

---
+++ Joinme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Joinme
+//
+//  Created by Merliah Ding on 2026/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Joinme/JoinmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoinmeApp.swift
+//  Joinme
+//
+//  Created by Merliah Ding on 2026/7/15.
+//
+
+import SwiftUI
+
+@main
+struct JoinmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
